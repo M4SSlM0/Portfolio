ef378f9a9ed6ee02575bee46cd74652caf6f0222
M4SSlM0 <103242350+M4SSlM0@users.noreply.github.com>
2024-04-26 18:39:13 +0200

    template progetti personali
    lavori in corso

---
+++ MyProjects/index.php
@@ -0,0 +1,54 @@
+<div class="myProjects-container">
+  <div class="myProjects-form-container">
+    <div class="myProjects-search-form-container">
+      <div class="myProjects-search-title">Search</div>
+      <div class="myProjects-search-form">
+        <div class="myProjects-search-input-container">
+          <div class="myProjects-input-label">Type</div>
+          <div class="custom-select">
+            <select name="" id="">
+              <option value="" selected>All</option>
+              <option value="">Illustrazione</option>
+              <option value="">Icona</option>
+              <option value="">Animazione</option>
+              <option value="">Asset</option>
+            </select>
+          </div>
+        </div>
+        <div class="myProjects-search-input-container">
+          <div class="myProjects-input-label">Description</div>
+          <div>
+            <input class="myProjects-search-input" type="text" name="" id="" />
+            <button class="myProjects-search-submit">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="display:flex;align-items:center;justify-content:center;"><button id="temp">FuckYouJs</button></div>
+  </div>
+  <div class="myProjects-background">
+    <div class="myProjects-projects">
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="project-placeholder"></div>
+      <div class="myProjects-background-footer">
+        <button><</button>
+        X out of Y
+        <button>></button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include "../Modals/customModal.html" ?>
+<script>
+  showModal(document.getElementById("temp"), "click");
+</script>
