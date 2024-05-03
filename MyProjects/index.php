cdf97f6704f9fa117d31631848401e4983ab9514
M4SSlM0 <103242350+M4SSlM0@users.noreply.github.com>
2024-05-03 20:44:29 +0200

    Progetti personali

--- MyProjects/index.php
+++ MyProjects/index.php
@@ -1,3 +1,10 @@
+<?php
+  $dbHost = "localhost";
+  $dbUser = "root";
+  $dbPass = "";
+  $dbName = "3dprojectdb";
+  $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+?>
 <div class="myProjects-container">
   <div class="myProjects-form-container">
     <div class="myProjects-search-form-container">
@@ -6,20 +13,23 @@
         <div class="myProjects-search-input-container">
           <div class="myProjects-input-label">Type</div>
           <div class="custom-select">
-            <select name="" id="">
-              <option value="" selected>All</option>
-              <option value="">Illustrazione</option>
-              <option value="">Icona</option>
-              <option value="">Animazione</option>
-              <option value="">Asset</option>
+            <select name="search-type" id="myProjects-search-type">
+              <option value="default" selected>All</option>
+              <?php
+                $results = $conn->query("SELECT * FROM tipi");
+                while($row = $results->fetch_array(MYSQLI_ASSOC)){?>
+                  <option value="<?= $row["ID"] ?>"><?= $row["Nome"] ?></option>
+                <?php
+                }
+              ?>
             </select>
           </div>
         </div>
         <div class="myProjects-search-input-container">
           <div class="myProjects-input-label">Description</div>
           <div>
-            <input class="myProjects-search-input" type="text" name="" id="" />
-            <button class="myProjects-search-submit">Search</button>
+            <input class="myProjects-search-input" type="text" name="search-description" id="myProjects-search-description" />
+            <button class="myProjects-search-submit" hx-post="../MyProjects/search.php" hx-trigger="click" hx-target="#results" hx-swap="innerHTML" hx-include="#myProjects-search-type, #myProjects-search-description">Search</button>
           </div>
         </div>
       </div>
@@ -27,24 +37,8 @@
     <div style="display:flex;align-items:center;justify-content:center;"><button id="new-project" hx-post="../Modals/modalNewProject.html" hx-trigger="click" hx-target="#custom-modal-body" hx-swap="innerHTML">+ New Project</button></div>
   </div>
   <div class="myProjects-background">
-    <div class="myProjects-projects">
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="project-placeholder"></div>
-      <div class="myProjects-background-footer">
-        <button><</button>
-        X out of Y
-        <button>></button>
-      </div>
+    <div class="myProjects-results" id="results">
+      <div hx-post="../MyProjects/search.php" hx-trigger="load" hx-target="this" hx-swap="outerHTML"></div>
     </div>
   </div>
 </div>
